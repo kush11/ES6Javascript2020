c4307d0d7359e60ef0ff40c7ac7b8dfde7f457b9
kush11 <kush.codingmart.com>
2020-02-07 15:04:40 +0530

    filter array helper function

--- Array_Helper_Method/filter.js
+++ Array_Helper_Method/filter.js
@@ -1,17 +1,43 @@
-var product = [
-    { name: 'Cucumber', type: 'vegitable' },
-    { name: 'Tomato', type: 'vegitable' },
-    { name: 'Apple', type: 'fruits' },
-    { name: 'Onion', type: 'vegitable' },
-    { name: 'Banana', type: 'fruits' }
+var products = [
+    { name: 'Cucumber', type: 'vegitable', quantity: 1, price: 1 },
+    { name: 'Tomato', type: 'vegitable', quantity: 6, price: 20 },
+    { name: 'Apple', type: 'fruits', quantity: 2, price: 15 },
+    { name: 'Onion', type: 'vegitable', quantity: 10, price: 12 },
+    { name: 'Banana', type: 'fruits', quantity: 3, price: 5 }
 ];
 
 var filterArray = [];
 
-for (var i = 0; i < product.length; i++) {
-    if (product[i].type === 'fruits') {
-        filterArray.push(product[i])
+for (var i = 0; i < products.length; i++) {
+    if (products[i].type === 'fruits') {
+        filterArray.push(products[i])
     }
-    // console.log(product[i].type)
+    // console.log(products[i].type)
 }
 console.log(filterArray)
+
+var filterArray1 = {}
+filterArray1 = products.filter((product) => {
+    return product.type === 'vegitable'
+})
+
+console.log('filterArray1', filterArray1)
+
+// filter type = vegitable quantity > 0 , price < 10
+console.log('sasasas', products.filter((product) => {
+    return product.type === 'vegitable' && product.quantity > 0 && product.price < 10
+}))
+
+var post = { id: 4, title: 'New Post' }
+var comments = [
+    { postId: 4, content: 'awsome post' },
+    { postId: 3, content: 'ok' },
+    { postId: 4, content: 'god' },
+];
+
+function commentsForPost(post, comments) {
+    return comments.filter((comment) => {
+        return comment.postId === post.id
+    });
+}
+console.log(commentsForPost(post, comments));
